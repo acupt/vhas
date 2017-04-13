816d57cbe999d7e23800fb5d04da05cc24d812af
liujie <liujie@taovip.com>
2017-04-13 20:03:33 +0800

    no2

--- src/main/java/com/swust/vhas/model/Video.java
+++ src/main/java/com/swust/vhas/model/Video.java
@@ -4,293 +4,293 @@ import java.util.Date;
 
 public class Video {
 
-	private Integer id;
+    private Integer id;
 
-	private Integer webId;
+    private Integer webId;
 
-	private Integer authorId;
+    private Integer authorId;
 
-	private Integer vlistId;
+    private Integer vlistId;
 
-	private String vid;
+    private String vid;
 
-	private String url;
+    private String url;
 
-	private String title;
+    private String title;
 
-	private String length;
+    private String length;
 
-	private Date time;
+    private Date time;
 
-	private Date uptime;
+    private Date uptime;
 
-	private Integer comment;
+    private Integer comment;
 
-	private Integer click;
+    private Integer click;
 
-	private Integer share;
+    private Integer share;
 
-	private Integer danmu;
+    private Integer danmu;
 
-	private Integer favorite;
+    private Integer favorite;
 
-	private Integer like;
+    private Integer like;
 
-	private Integer dislike;
+    private Integer dislike;
 
-	private Integer coin;
+    private Integer coin;
 
-	private String type;
+    private String type;
 
-	private Integer typeidweb;
+    private Integer typeidweb;
 
-	private Integer typeId;
+    private Integer typeId;
 
-	private String author;
+    private String author;
 
-	private String uid;
+    private String uid;
 
-	private String tag;
+    private String tag;
 
-	private String copyright;
+    private String copyright;
 
-	private String img;
+    private String img;
 
-	private String director;
+    private String director;
 
-	private String actor;
+    private String actor;
 
-	private Integer video_id;
+    private Integer video_id;
 
-	public Integer getVideo_id() {
-		return video_id;
-	}
+    public Integer getVideo_id() {
+        return video_id;
+    }
 
-	public void setVideo_id(Integer video_id) {
-		this.video_id = video_id;
-	}
+    public void setVideo_id(Integer video_id) {
+        this.video_id = video_id;
+    }
 
-	public Integer getId() {
-		return id;
-	}
+    public Integer getId() {
+        return id;
+    }
 
-	public void setId(Integer id) {
-		this.id = id;
-	}
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
-	public Integer getWebId() {
-		return webId;
-	}
+    public Integer getWebId() {
+        return webId;
+    }
 
-	public void setWebId(Integer webId) {
-		this.webId = webId;
-	}
+    public void setWebId(Integer webId) {
+        this.webId = webId;
+    }
 
-	public Integer getAuthorId() {
-		return authorId;
-	}
+    public Integer getAuthorId() {
+        return authorId;
+    }
 
-	public void setAuthorId(Integer authorId) {
-		this.authorId = authorId;
-	}
+    public void setAuthorId(Integer authorId) {
+        this.authorId = authorId;
+    }
 
-	public Integer getVlistId() {
-		return vlistId;
-	}
+    public Integer getVlistId() {
+        return vlistId;
+    }
 
-	public void setVlistId(Integer vlistId) {
-		this.vlistId = vlistId;
-	}
+    public void setVlistId(Integer vlistId) {
+        this.vlistId = vlistId;
+    }
 
-	public String getVid() {
-		return vid;
-	}
-
-	public void setVid(String vid) {
-		this.vid = vid == null ? null : vid.trim();
-	}
-
-	public String getUrl() {
-		return url;
-	}
-
-	public void setUrl(String url) {
-		this.url = url == null ? null : url.trim();
-	}
-
-	public String getTitle() {
-		return title;
-	}
-
-	public void setTitle(String title) {
-		this.title = title == null ? null : title.trim();
-	}
-
-	public String getLength() {
-		return length;
-	}
-
-	public void setLength(String length) {
-		this.length = length == null ? null : length.trim();
-	}
-
-	public Date getTime() {
-		return time;
-	}
-
-	public void setTime(Date time) {
-		this.time = time;
-	}
-
-	public Date getUptime() {
-		return uptime;
-	}
-
-	public void setUptime(Date uptime) {
-		this.uptime = uptime;
-	}
-
-	public Integer getComment() {
-		return comment;
-	}
-
-	public void setComment(Integer comment) {
-		this.comment = comment;
-	}
-
-	public Integer getClick() {
-		return click;
-	}
-
-	public void setClick(Integer click) {
-		this.click = click;
-	}
-
-	public Integer getShare() {
-		return share;
-	}
-
-	public void setShare(Integer share) {
-		this.share = share;
-	}
-
-	public Integer getDanmu() {
-		return danmu;
-	}
-
-	public void setDanmu(Integer danmu) {
-		this.danmu = danmu;
-	}
-
-	public Integer getFavorite() {
-		return favorite;
-	}
-
-	public void setFavorite(Integer favorite) {
-		this.favorite = favorite;
-	}
-
-	public Integer getLike() {
-		return like;
-	}
-
-	public void setLike(Integer like) {
-		this.like = like;
-	}
-
-	public Integer getDislike() {
-		return dislike;
-	}
-
-	public void setDislike(Integer dislike) {
-		this.dislike = dislike;
-	}
-
-	public Integer getCoin() {
-		return coin;
-	}
-
-	public void setCoin(Integer coin) {
-		this.coin = coin;
-	}
-
-	public String getType() {
-		return type;
-	}
-
-	public void setType(String type) {
-		this.type = type == null ? null : type.trim();
-	}
-
-	public Integer getTypeidweb() {
-		return typeidweb;
-	}
-
-	public void setTypeidweb(Integer typeidweb) {
-		this.typeidweb = typeidweb;
-	}
-
-	public Integer getTypeId() {
-		return typeId;
-	}
-
-	public void setTypeId(Integer typeId) {
-		this.typeId = typeId;
-	}
-
-	public String getAuthor() {
-		return author;
-	}
-
-	public void setAuthor(String author) {
-		this.author = author == null ? null : author.trim();
-	}
-
-	public String getUid() {
-		return uid;
-	}
-
-	public void setUid(String uid) {
-		this.uid = uid == null ? null : uid.trim();
-	}
-
-	public String getTag() {
-		return tag;
-	}
-
-	public void setTag(String tag) {
-		this.tag = tag == null ? null : tag.trim();
-	}
-
-	public String getCopyright() {
-		return copyright;
-	}
-
-	public void setCopyright(String copyright) {
-		this.copyright = copyright == null ? null : copyright.trim();
-	}
-
-	public String getImg() {
-		return img;
-	}
-
-	public void setImg(String img) {
-		this.img = img == null ? null : img.trim();
-	}
-
-	public String getDirector() {
-		return director;
-	}
-
-	public void setDirector(String director) {
-		this.director = director == null ? null : director.trim();
-	}
-
-	public String getActor() {
-		return actor;
-	}
-
-	public void setActor(String actor) {
-		this.actor = actor == null ? null : actor.trim();
-	}
+    public String getVid() {
+        return vid;
+    }
+
+    public void setVid(String vid) {
+        this.vid = vid == null ? null : vid.trim();
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url == null ? null : url.trim();
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title == null ? null : title.trim();
+    }
+
+    public String getLength() {
+        return length;
+    }
+
+    public void setLength(String length) {
+        this.length = length == null ? null : length.trim();
+    }
+
+    public Date getTime() {
+        return time;
+    }
+
+    public void setTime(Date time) {
+        this.time = time;
+    }
+
+    public Date getUptime() {
+        return uptime;
+    }
+
+    public void setUptime(Date uptime) {
+        this.uptime = uptime;
+    }
+
+    public Integer getComment() {
+        return comment;
+    }
+
+    public void setComment(Integer comment) {
+        this.comment = comment;
+    }
+
+    public Integer getClick() {
+        return click;
+    }
+
+    public void setClick(Integer click) {
+        this.click = click;
+    }
+
+    public Integer getShare() {
+        return share;
+    }
+
+    public void setShare(Integer share) {
+        this.share = share;
+    }
+
+    public Integer getDanmu() {
+        return danmu;
+    }
+
+    public void setDanmu(Integer danmu) {
+        this.danmu = danmu;
+    }
+
+    public Integer getFavorite() {
+        return favorite;
+    }
+
+    public void setFavorite(Integer favorite) {
+        this.favorite = favorite;
+    }
+
+    public Integer getLike() {
+        return like;
+    }
+
+    public void setLike(Integer like) {
+        this.like = like;
+    }
+
+    public Integer getDislike() {
+        return dislike;
+    }
+
+    public void setDislike(Integer dislike) {
+        this.dislike = dislike;
+    }
+
+    public Integer getCoin() {
+        return coin;
+    }
+
+    public void setCoin(Integer coin) {
+        this.coin = coin;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type == null ? null : type.trim();
+    }
+
+    public Integer getTypeidweb() {
+        return typeidweb;
+    }
+
+    public void setTypeidweb(Integer typeidweb) {
+        this.typeidweb = typeidweb;
+    }
+
+    public Integer getTypeId() {
+        return typeId;
+    }
+
+    public void setTypeId(Integer typeId) {
+        this.typeId = typeId;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author == null ? null : author.trim();
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid == null ? null : uid.trim();
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag == null ? null : tag.trim();
+    }
+
+    public String getCopyright() {
+        return copyright;
+    }
+
+    public void setCopyright(String copyright) {
+        this.copyright = copyright == null ? null : copyright.trim();
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img == null ? null : img.trim();
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director == null ? null : director.trim();
+    }
+
+    public String getActor() {
+        return actor;
+    }
+
+    public void setActor(String actor) {
+        this.actor = actor == null ? null : actor.trim();
+    }
 }
